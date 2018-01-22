20908e97395089ec9103b4af81ff951044b7423d
amirahnazli <33883963+amirahnazli@users.noreply.github.com>
2018-01-22 10:33:36 +0800

    TASK COMPLETE

--- String-Exercise-1.cpp
+++ String-Exercise-1.cpp
@@ -1,15 +1,28 @@
+//NUR AMIRAH AFIQAH
+//A17DW2172
 //include your name and matric number here
 //display the string below using the string function state below.
 
 #include <iostream>
+#include <cstring>
+using namespace std;
 
 int main()
 {
-	char engkau[10];
+	char engkau[10] = "HELLO";
+	char awak[10] = "WORLD";
+	char kita[10];
+	int len;
 
 	//stringcopy(a,"hello");
+	strcpy (kita, engkau);
+	cout<< "strcpy (kita, engkau) : " <<kita <<endl;
 	
-	cout<<engkau;
+	strcat (engkau, awak);
+	cout<< "strcat (engkau, awak) : " <<engkau <<endl;
+	
+	len = strlen (engkau);
+	cout<< "strlen (engkau) : " <<len <<endl;
 
 	return 0;
 }
